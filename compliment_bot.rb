cbd637ba12fe36c660cbf96ddb75344d59ee6165
sevoo <sev@byz.org>
2018-09-08 19:10:54 -0700

    Add files via upload
    initial commit

---
+++ compliment_bot.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'mastodon'
+
+BEARER_TOKEN = ENV['CB_BEARER_TOKEN']
+client = Mastodon::REST::Client.new(base_url: "https://botsin.space", bearer_token: BEARER_TOKEN)
+
+class ComplimentBot
+  def initialize
+    @nouns = ["elbow", "fashion sense", "intuition", "favorite", "sunset", "protocol", "hand", "nose", "toot"]
+    @adjectives = ["nifty", "lovely", "creative", "my favorite", "epic", "super", "cool", "superb", "spectacular"]
+  end
+
+  def compliment(subject)
+    noun = @nouns.sample
+    adj = @adjectives.sample
+    "#{subject} #{noun} is #{adj}!"
+  end
+
+  def praise
+    # pick a name
+    compliment("#{name}'s")
+  end
+
+end
+
+bot = ComplimentBot.new
+
+while true
+  client.create_status(bot.compliment("Your"))
+  sleep(3600)
+end
